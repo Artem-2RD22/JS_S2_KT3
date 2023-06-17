f41efbf9768b4aefb1d813ae4be4e62c6294f90d
Artem-2RD22 <127699457+Artem-2RD22@users.noreply.github.com>
2023-06-17 15:13:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let isDragging = false;
+const slider = document.querySelector('.slider');
+const valueDisplay = document.querySelector('.slider-value');
+const tickmarks = document.querySelector('.tickmarks');
+const sliderContainer = document.querySelector('.slider-container');
+const sliderWidth = sliderContainer.clientWidth;
+
+// начальное положение слайдера и значения
+let sliderPosition = 0;
+slider.style.left = `${sliderPosition - 8}px`;
+valueDisplay.style.left = `${sliderPosition + 1}px`;
+valueDisplay.textContent = Math.round((sliderPosition / sliderWidth) * 100);
+
+// нажатие мыши
+slider.addEventListener('mousedown', (event) => {
+    isDragging = true;
+});
+
+// отслеживание движения мыши
+document.addEventListener('mousemove', (event) => {
+    if (isDragging) {
+        // вычисление положение слайдера по положению курсора
+        let mouseX = event.clientX - sliderContainer.offsetLeft;
+        sliderPosition = Math.max(0, Math.min(mouseX, sliderWidth));
+
+        // обновление положение слайдера и значения
+        slider.style.left = `${sliderPosition - 8}px`;
+        valueDisplay.style.left = `${sliderPosition + 1}px`;
+        valueDisplay.textContent = Math.round((sliderPosition / sliderWidth) * 100);
+    }
+});
+
+// отпускание мыши
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+});
+
+// метки на оси
+const tickmarkValues = [0, 25, 50, 75, 100];
+// добавление меток
+tickmarkValues.forEach((value) => {
+    const tickmark = document.createElement('div');
+    tickmark.classList.add('tickmark');
+    tickmark.style.left = `${value}%`;
+
+    const tickmarkLabel = document.createElement('div');
+    tickmarkLabel.classList.add('tickmark-label');
+    tickmarkLabel.textContent = value;
+    tickmark.appendChild(tickmarkLabel);
+
+    tickmarks.appendChild(tickmark);
+});
